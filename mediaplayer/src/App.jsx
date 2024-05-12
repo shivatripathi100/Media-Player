1313a42a80c38c47c48bebfa2212fdd93ff2ed3a
shivatripathi <shiva100tripathi@gmail.com>
2024-05-13 00:20:38 +0530

    Initial Commit

---
+++ mediaplayer/src/App.jsx
@@ -0,0 +1,258 @@
+import React, { useState, useRef, useEffect } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import ReactPlayer from 'react-player';
+import { styled } from '@mui/system';
+import PlayerControls from './Components/PlayerControls';
+import screenfull from 'screenfull';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
+const PlayerWrapper = styled('div')({
+  width: '100%',
+  position: 'relative',
+});
+
+const format = (seconds) => {
+  if (isNaN(seconds)) {
+    return `00:00`;
+  }
+  const date = new Date(seconds * 1000);
+  const hh = date.getUTCHours();
+  const mm = date.getUTCMinutes();
+  const ss = date.getUTCSeconds().toString().padStart(2, '0');
+  if (hh) {
+    return `${hh}:${mm.toString().padStart(2, '0')}:${ss}`;
+  }
+  return `${mm}:${ss}`;
+};
+
+function App() {
+  const [state, setState] = useState({
+    playing: true,
+    muted: true,
+    volume: 0.5,
+    playbackRate: 1.0,
+    played: 0,
+    seeking: false,
+    currentVideoIndex: 0,
+  });
+
+  const [timeDisplayFormat, setTimeDisplayFormat] = useState('normal');
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const { playing, muted, volume, playbackRate, played, seeking, currentVideoIndex } = state;
+
+  const playerRef = useRef(null);
+  const playerContainerRef = useRef(null);
+  const canvasRef = useRef(null);
+  const controlsRef = useRef(null);
+
+  const videos = [
+    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4'
+  ];
+
+  const handlePlayPause = () => {
+    setState({ ...state, playing: !state.playing });
+  };
+
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
+  };
+
+  const handleFastForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10);
+  };
+
+  const handleMute = () => {
+    setState({ ...state, muted: !state.muted });
+  };
+
+  const handleVolumeSeekUp = (e, newValue) => {
+    setState({ ...state, seeking: false, volume: parseFloat(newValue / 100) });
+  };
+
+  const handleVolumeChange = (e, newValue) => {
+    setState({
+      ...state,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
+    });
+  };
+
+  const handlePlaybackRateChange = (rate) => {
+    setState({ ...state, playbackRate: rate });
+  };
+
+  const toggleFullScreen = () => {
+    screenfull.toggle(playerContainerRef.current);
+  };
+
+  const handleProgress = (changeState) => {
+    if (!state.seeking) {
+      setState({ ...state, ...changeState });
+    }
+  };
+
+  const handleSeekChange = (e, newValue) => {
+    setState({ ...state, played: parseFloat(newValue / 100) });
+  };
+
+  const handleSeekMouseDown = (e) => {
+    setState({ ...state, seeking: true });
+  };
+
+  const handleSeekMouseUp = (e, newValue) => {
+    setState({ ...state, seeking: false });
+    playerRef.current.seekTo(newValue / 100, 'fraction');
+  };
+
+  const handleChangeDisplayFormat = () => {
+    setTimeDisplayFormat(timeDisplayFormat === 'normal' ? 'remaining' : 'normal');
+  };
+
+  const addBookmark = () => {
+    const canvas = canvasRef.current;
+    canvas.width = 160;
+    canvas.height = 90;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      playerRef.current.getInternalPlayer(),
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+    const dataUri = canvas.toDataURL();
+    canvas.width = 0;
+    canvas.height = 0;
+    const bookmarksCopy = [...bookmarks];
+    bookmarksCopy.push({
+      time: playerRef.current.getCurrentTime(),
+      display: format(playerRef.current.getCurrentTime()),
+      image: dataUri,
+    });
+    setBookmarks(bookmarksCopy);
+  };
+
+  const handleNextVideo = () => {
+    setState(prevState => ({
+      ...prevState,
+      currentVideoIndex: (prevState.currentVideoIndex + 1) % videos.length,
+      
+    }));
+  };
+
+  const handlePreviousVideo = () => {
+    setState(prevState => ({
+      ...prevState,
+      currentVideoIndex: (prevState.currentVideoIndex - 1 + videos.length) % videos.length,
+    }));
+  };
+
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (!state.seeking && playing) {
+        setState((prevState) => ({
+          ...prevState,
+          played: playerRef.current.getCurrentTime() / playerRef.current.getDuration(),
+        }));
+      }
+    }, 1000); 
+
+    return () => clearInterval(intervalId);
+  }, [playing, state.seeking]);
+
+  const currentTime = playerRef.current ? playerRef.current.getCurrentTime() : '00:00';
+  const duration = playerRef.current ? playerRef.current.getDuration() : '00:00';
+  const elapsedTime =
+    timeDisplayFormat === 'normal' ? format(currentTime) : `-${format(duration - currentTime)}`;
+  const totalDuration = format(duration);
+
+  return (
+    <>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography variant='h6'>React Media Player</Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container maxWidth='md'>
+        <PlayerWrapper ref={playerContainerRef}>
+          <ReactPlayer
+            ref={playerRef}
+            width={'100%'}
+            height={'100%'}
+            url={videos[currentVideoIndex]} 
+            muted={muted}
+            playing={playing}
+            volume={volume}
+            playbackRate={playbackRate}
+            onProgress={handleProgress}
+            config={{
+              file: {
+                attributes: {
+                  crossOrigin: 'anonymous',
+                },
+              },
+            }}
+          />
+
+          <PlayerControls
+            ref={controlsRef}
+            onPlayPause={handlePlayPause}
+            playing={playing}
+            onRewind={handleRewind}
+            onFastForward={handleFastForward}
+            muted={muted}
+            onMute={handleMute}
+            onVolumeChange={handleVolumeChange}
+            onVolumeSeekUp={handleVolumeSeekUp}
+            volume={volume}
+            playbackRate={playbackRate}
+            onPlaybackRateChange={handlePlaybackRateChange}
+            onToggleFullScreen={toggleFullScreen}
+            played={played}
+            onSeek={handleSeekChange}
+            onSeekMouseDown={handleSeekMouseDown}
+            onSeekMouseUp={handleSeekMouseUp}
+            elapsedTime={elapsedTime}
+            totalDuration={totalDuration}
+            onChangeDisplayFormat={handleChangeDisplayFormat}
+            onBookmark={addBookmark}
+            onNextVideo={handleNextVideo} 
+            onPreviousVideo={handlePreviousVideo} 
+          />
+        </PlayerWrapper>
+
+        <Grid container style={{ marginTop: 20 }} spacing={3}>
+          {bookmarks.map((bookmark, index) => (
+            <Grid key={index} item>
+              <Paper
+                onClick={() => {
+                  playerRef.current.seekTo(bookmark.time);
+                }}
+                elevation={3}
+              >
+                <img crossOrigin='anonymous' src={bookmark.image} alt='bookmark' />
+                <Typography variant='body2' align='center'>
+                  Bookmark at {bookmark.display}
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+        <canvas ref={canvasRef} style={{ display: 'none' }} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ mediaplayer/src/Components/PlayerControls.jsx
@@ -0,0 +1,253 @@
+import React, { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import { styled } from "@mui/system";
+import Grid from '@mui/material/Grid';
+import Bookmark from '@mui/icons-material/Bookmark';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import FastRewindIcon from '@mui/icons-material/FastRewind';
+import FastForwardIcon from '@mui/icons-material/FastForward';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import Slider from '@mui/material/Slider';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import FullScreenIcon from '@mui/icons-material/FullScreen';
+import Popover from '@mui/material/Popover';
+import VolumeOff from '@mui/icons-material/VolumeOff';
+
+const ControlWrapper = styled('div')({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  background: 'rgba(0,0,0,0.6)',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  zIndex: 1
+});
+
+const ControlIcons = styled(IconButton)({
+  color: '#777',
+  fontSize: 50,
+  transform: 'scale(0.9)',
+  '&:hover': {
+    color: '#fff',
+    transform: 'scale(1)'
+  },
+});
+
+const ControlVideoIcons = styled(IconButton)({
+  color: '#777',
+  fontSize: 50,
+  transform: 'scale(0.9)',
+  '&:hover': {
+    color: '#fff',
+    transform: 'scale(1)'
+  },
+});
+
+const BottomIcons = styled(IconButton)({
+  color: '#999',
+  '&:hover': {
+    colors: '#fff'
+  },
+});
+
+const VolumeSlider = styled(Slider)({
+  width: 100,
+});
+
+const PlayControls = React.forwardRef(({
+  onPlayPause,
+  playing,
+  onRewind,
+  onFastForward,
+  muted,
+  onMute,
+  onVolumeChange,
+  onVolumeSeekUp,
+  volume,
+  onPlaybackRateChange,
+  playbackRate,
+  onToggleFullScreen,
+  played,
+  onSeek,
+  onSeekMouseDown,
+  onSeekMouseUp,
+  elapsedTime,
+  totalDuration,
+  onChangeDisplayFormat,
+  onBookmark,
+  onNextVideo,
+  onPreviousVideo
+}, ref) => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [seekValue, setSeekValue] = useState(played * 100);
+  const [volumeValue, setVolumeValue] = useState(volume * 100);
+
+  const handlePopover = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'playbackrate-popover' : undefined;
+
+  return (
+    <ControlWrapper ref={ref}>
+      {/* Top Controls */}
+      <Grid
+        container direction='row'
+        alignItems='center'
+        justify='space-between'
+        style={{ padding: 16 }}>
+        <Grid item>
+          <Typography variant='h5' style={{ color: '#fff' }}>
+            Video Title
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <Button
+            onClick={onBookmark}
+            variant='contained'
+            color='primary'
+            startIcon={<Bookmark />}
+            style={{ marginLeft: '400%' }}
+          >
+            Bookmark
+          </Button>
+        </Grid>
+      </Grid>
+
+      {/* Middle Controls */}
+      <Grid
+        container
+        direction='row'
+        alignItems='center'
+        justify='center'
+        style={{ marginLeft: '35%' }}
+      >
+        
+
+        <ControlIcons onClick={onRewind} aria-label='rewind'>
+          <FastRewindIcon fontSize='inherit' />
+        </ControlIcons>
+
+        <ControlIcons onClick={onPlayPause} aria-label='play/pause'>
+          {playing ? (
+            <PauseIcon fontSize='inherit' />
+          ) : (
+            <PlayArrowIcon fontSize='inherit' />
+          )}
+        </ControlIcons>
+
+        <ControlIcons onClick={onFastForward} aria-label='fast forward'>
+          <FastForwardIcon fontSize='inherit' />
+        </ControlIcons>
+
+        
+      </Grid>
+
+      {/* Bottom controls */}
+      <Grid
+        container
+        direction='row'
+        justify='space-between'
+        alignItems='center'
+        style={{ padding: 16 }}
+      >
+        <Grid item xs={12}>
+          <Slider
+            min={0}
+            max={100}
+            value={played * 100} 
+            onChange={onSeek}
+            onMouseDown={onSeekMouseDown}
+            onChangeCommitted={onSeekMouseUp}	
+          />
+        </Grid>
+
+        <Grid item>
+          <Grid container alignItems='center' direction='row'>
+            <BottomIcons onClick={onPlayPause}>
+              {playing ? (
+                <PauseIcon fontSize='inherit' />
+              ) : (
+                <PlayArrowIcon fontSize='inherit' />
+              )}
+            </BottomIcons>
+
+            <BottomIcons onClick={onMute}>
+              {muted ? <VolumeOff fontSize='large' /> : <VolumeUpIcon fontSize='large' />}
+            </BottomIcons>
+
+            <VolumeSlider
+              min={0}
+              max={100}
+              value={volumeValue}
+              onChange={(event, newValue) => {
+                setVolumeValue(newValue);
+                onVolumeChange(event, newValue);
+              }}
+              onChangeCommitted={(event, newValue) => onVolumeSeekUp(event, newValue)}
+            />
+            <Button onClick={onChangeDisplayFormat}>
+              <Typography
+                variant='text'
+                style={{ color: '#fff', marginLeft: 16 }}
+              >
+                {elapsedTime}/{totalDuration}
+              </Typography>
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button onClick={handlePopover} variant='text'>
+            <Typography>{playbackRate}X</Typography>
+          </Button>
+
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+          >
+            <Grid container direction='column'></Grid>
+            {[0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 4.0].map(rate => (
+              <Button key={rate} onClick={() => onPlaybackRateChange(rate)} variant='text'>
+                <Typography color={rate === playbackRate ? "secondary" : "default"}>{rate}</Typography>
+              </Button>
+            ))}
+          </Popover>
+          <BottomIcons onClick={onToggleFullScreen}>
+            <FullScreenIcon fontSize='large' />
+          </BottomIcons>
+        </Grid>
+
+        <ControlVideoIcons onClick={onPreviousVideo} aria-label='previous'>
+          <SkipPreviousIcon fontSize='inherit' />
+        </ControlVideoIcons>
+
+        <ControlVideoIcons onClick={onNextVideo} aria-label='next'>
+          <SkipNextIcon fontSize='inherit' />
+        </ControlVideoIcons>
+
+      </Grid>
+    </ControlWrapper>
+  );
+});
+
+export default PlayControls;
